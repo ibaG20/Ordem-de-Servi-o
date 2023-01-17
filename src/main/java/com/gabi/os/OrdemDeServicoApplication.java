9c7bc7d66aa06330bc360dd735b71fb1dd2246b2
gamg <gamg@gft.com>
2023-01-17 11:21:59 -0300

    teste de deploy

--- src/main/java/com/gabi/os/OrdemDeServicoApplication.java
+++ src/main/java/com/gabi/os/OrdemDeServicoApplication.java
@@ -11,3 +11,15 @@ public class OrdemDeServicoApplication{
 	}
 
 }
+
+/*
+spring_profiles_active=dev
+PROD_DB_URL=mysql://${{ MYSQLUSER }}:${{ MYSQLPASSWORD }}@${{ MYSQLHOST }}:${{ MYSQLPORT }}/${{ MYSQLDATABASE }}
+PROD_DB_URL=mysql://${{ root }}:${{ xJQj3f7jHB4ygrotvsgK }}@${{ containers-us-west-193.railway.app }}:${{ 6683 }}/${{ railway }}
+PROD_DB_HOST=containers-us-west-193.railway.app
+PROD_DB_PORT=6683
+PROD_DB_NAME=railway
+PROD_DB_PASSWORD=xJQj3f7jHB4ygrotvsgK
+PROD_DB_USERNAME=root
+ 
+ */
