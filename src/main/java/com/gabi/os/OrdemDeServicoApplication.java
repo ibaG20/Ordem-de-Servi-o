9b9324884a8e896a561453a29be87c59411ec076
gamg <gamg@gft.com>
2023-01-23 16:27:52 -0300

    novo commit

--- src/main/java/com/gabi/os/OrdemDeServicoApplication.java
+++ src/main/java/com/gabi/os/OrdemDeServicoApplication.java
@@ -16,9 +16,10 @@ public class OrdemDeServicoApplication{
 spring_profiles_active=prod
 PROD_DB_URL=mysql://${{ MYSQLUSER }}:${{ MYSQLPASSWORD }}@${{ MYSQLHOST }}:${{ MYSQLPORT }}/${{ MYSQLDATABASE }}
 PROD_DB_URL=mysql://${{ root }}:${{ xJQj3f7jHB4ygrotvsgK }}@${{ containers-us-west-193.railway.app }}:${{ 6683 }}/${{ railway }}
+spring_profiles_active=prod
 PROD_DB_HOST=containers-us-west-193.railway.app
-PROD_DB_PORT=6683
+PROD_DB_PORT=6523
 PROD_DB_NAME=railway
-PROD_DB_PASSWORD=xJQj3f7jHB4ygrotvsgK
-PROD_DB_USERNAME=root
+PROD_DB_PASSWORD=I5evpgIUOLueDqwJCfUT
+PROD_DB_USERNAME=postgres
 */
